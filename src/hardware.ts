03e04183cba255fc111ba23c338fd32424298fd6
Gavin Scott <gavin.o.scott@gmail.com>
2024-03-08 23:50:16 -0800

    Store previous CPU/Memory data, respond with array

--- src/hardware.ts
+++ src/hardware.ts
@@ -1,18 +1,24 @@
 import { cpu as cpuUtil, mem } from 'node-os-utils'
 
-export let cpu: number = -1
-export let memory: number = -1
+export let cpu: number[] = []
+export let memory: number[] = []
+
+const MAX_CACHED_VALUES = 20
+
+const addToArray = (value: number, array: number[]) => {
+  return array.length >= MAX_CACHED_VALUES ? [...array.slice(1), value] : [...array, value]
+}
 
 const getCpuUsage = () => {
   cpuUtil.usage().then((_) => {
-    cpu = _
+    cpu = addToArray(_, cpu)
     getCpuUsage()
   })
 }
 
 const getMemoryUsage = () => {
   mem.info().then((_) => {
-    memory = _.usedMemPercentage
+    memory = addToArray(_.usedMemPercentage, memory)
     getMemoryUsage()
   })
 }
